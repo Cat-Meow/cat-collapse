064b3088c4f01805b06987d8c6d7586e2c4c736b
yanweiqing <yanweiqing@youzan.com>
2016-03-28 17:25:31 +0800

    改进readme

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-# Cat-Collapse
+# React Salt - Collapse
 
 折叠面板组件依赖于panel
 
 对复杂区域进行分组和隐藏，保持页面的整洁。
+
+Accordion 提供手风琴功能，提供以下api：
+
+* liveKey: 当前需要展开的手风琴
+* auto: 自动折叠与打开手风琴
+* onSelect: 回调函数，可以告知选中的项目
+
+需要为Collapse组件设置eventKey作为该组件的标识
--- src/accordion.js
+++ src/accordion.js
@@ -1,78 +1,78 @@
 import React, {Component} from 'react';
 
 class Accordion extends Component {
-  static propTypes = {
-    liveKey: React.PropTypes.string,
-    auto: React.PropTypes.bool,
-    prefixName: React.PropTypes.string,
-    onSelect: React.PropTypes.func
-  }
-
-  static defaultProps = {
-    liveKey: '',
-    auto: false,
-    prefixName: 'salt'
-  }
-
-  _createLiveKey(props) {
-    return props.auto ? props.liveKey : [props.liveKey];
-  }
-
-  state = {
-    liveKey: this._createLiveKey(this.props)
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (this.state.liveKey !== nextProps.liveKey) {
-      this.setState({
-        liveKey: this.createLiveKey(nextProps)
-      });
-    }
-  }
-
-  handleClick(value){
-    let { liveKey } = this.state;
-
-    if (this.props.auto) {
-      liveKey = value;
-    } else {
-      let index = liveKey.indexOf(value);
-      if (index > -1) {
-        liveKey.splice(index, 1);
-      } else {
-        liveKey.push(value);
-      }
+    static propTypes = {
+        liveKey: React.PropTypes.string,
+        auto: React.PropTypes.bool,
+        prefixName: React.PropTypes.string,
+        onSelect: React.PropTypes.func
     }
 
-    this.setState({
-      liveKey: liveKey
-    });
+    static defaultProps = {
+        liveKey: '',
+        auto: false,
+        prefixName: 'salt'
+    }
 
-    this.props.onSelect && this.props.onSelect(value, liveKey);
-  }
+    _createLiveKey(props) {
+        return props.auto ? props.liveKey : [props.liveKey];
+    }
 
-  render() {
-    let { auto, prefixName } = this.props;
-    let { liveKey } = this.state;
-    let self = this;
+    state = {
+        liveKey: this._createLiveKey(this.props)
+    }
 
-    return (
-      <div className="panel-group">
-        {
-          React.Children.map(this.props.children, (child, index) => {
-            let { eventKey=index } = child.props;
-            return React.cloneElement(child, {
-              show: Array.isArray(liveKey) ?
-                liveKey.indexOf(eventKey) > -1 :
-                liveKey === eventKey,
-              prefixName: prefixName,
-              onClick: self.handleClick.bind(self, eventKey)
+    componentWillReceiveProps(nextProps) {
+        if (this.state.liveKey !== nextProps.liveKey) {
+            this.setState({
+                liveKey: this.createLiveKey(nextProps)
             });
-          })
         }
-      </div>
-    )
-  }
+    }
+
+    handleClick(value){
+        let { liveKey } = this.state;
+
+        if (this.props.auto) {
+            liveKey = value;
+        } else {
+            let index = liveKey.indexOf(value);
+            if (index > -1) {
+                liveKey.splice(index, 1);
+            } else {
+                liveKey.push(value);
+            }
+        }
+
+        this.setState({
+            liveKey: liveKey
+        });
+
+        this.props.onSelect && this.props.onSelect(value, liveKey);
+    }
+
+    render() {
+        let { auto, prefixName } = this.props;
+        let { liveKey } = this.state;
+        let self = this;
+
+        return (
+            <div className="panel-group">
+                {
+                    React.Children.map(this.props.children, (child, index) => {
+                        let { eventKey=index } = child.props;
+                        return React.cloneElement(child, {
+                            show: Array.isArray(liveKey) ?
+                                liveKey.indexOf(eventKey) > -1 :
+                                liveKey === eventKey,
+                            prefixName: prefixName,
+                            onClick: self.handleClick.bind(self, eventKey)
+                        });
+                    })
+                }
+            </div>
+        )
+    }
 }
 
 export default Accordion;
